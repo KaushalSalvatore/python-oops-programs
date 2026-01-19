f765a0099893b4255c266822be57076944936527
KaushalSalvatore <kaushalpandey455@gmail.com>
2026-01-19 22:15:06 +0530

    add hacker rank problem

---
+++ hackerrank Problem/problem-02.py
@@ -0,0 +1,21 @@
+'''
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+
+n = int(input())
+
+
+if(2 <=  n <= 5 and n%2==0):
+    print("Not Weird")
+elif(6 <=  n <= 20 and n%2==0):
+    print("Weird")
+elif(n > 20  and  n%2==0):
+    print("Not Weird")
+elif(n%2!=0):
+    print("Weird")
