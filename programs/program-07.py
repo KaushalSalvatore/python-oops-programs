4d4b6c1cb9badd45366e0e626f59756b172b86f9
KaushalSalvatore <kaushalpandey455@gmail.com>
2026-01-24 22:10:33 +0530

    new program

--- programs/program-07.py
+++ programs/program-07.py
@@ -1,3 +1,12 @@
 '''
-Remove character for string given input 
+Remove character form string given input 
 '''
+str1 = input()
+str2 = 'Remove character from String'.lower()
+for chr in str1.lower():
+    if chr in str2.lower():
+        str2 = str2.replace(chr,'')
+    else:
+        print("No String")
+
+print(str2)
--- programs/program-10.py
+++ programs/program-10.py
@@ -1,3 +1,11 @@
 '''
 character occurrence in string
 '''
+
+str1 = "character occurrence in string"
+dict = {}
+for chr in str1:
+     
+    dict[chr] = dict.get(chr,0)+1
+
+print(dict)
