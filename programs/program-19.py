3ac99bd18ea1ab2539e301767c136dfe1ad0c6bc
KaushalSalvatore <kaushalpandey455@gmail.com>
2026-02-02 17:52:40 +0530

    add new

--- programs/program-19.py
+++ programs/program-19.py
@@ -2,4 +2,28 @@
 Fibonacci Series using reursion and function
 (0,1,1,2,3,5,8,13,21,34,55,.....)
 '''
+# a = 0
+# b = 1 
+# n = 0
+# arr = [0,1]
+# while n < 10:
+#     c = a+b
+#     arr.append(c)
+#     a = b
+#     b = c
+#     n += 1
+# print(arr)
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(6):
+    print(fibonacci(i), end=" ")
+
+
+
+    
 
--- programs/program-20.py
+++ programs/program-20.py
@@ -2,3 +2,12 @@
 swap the array value according to given index
 '''
 
+arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+a = int(input())
+b = int(input())
+
+arr.remove(a)
+arr.remove(b)
+
+print(arr)
